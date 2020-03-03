77da5a7c2b863d625587653432daac228d900288
nivet <fun5xxx@gmail.com>
2020-03-03 21:11:42 +0800

    fix bugs

--- src/manage/Components/HeaderCustom.jsx
+++ src/manage/Components/HeaderCustom.jsx
@@ -7,7 +7,7 @@ import avatar from '../../static/avatar.jpg';
 import SiderCustom from './SiderCustom';
 import {connect} from 'react-redux';
 import {withRouter} from 'react-router-dom';
-import {getUser} from "../../utils/tools";
+import {getUser,removeUser,removeToken,removeRole} from "../../utils/tools";
 
 const {Header} = Layout;
 const SubMenu = Menu.SubMenu;
@@ -42,7 +42,10 @@ class HeaderCustom extends Component {
     logout = () => {
         // loginout().then((data) => {
         //     // localStorage.removeItem('user');
-        //     this.props.history.push('/login')
+        removeUser();
+        removeRole();
+        removeToken();
+        this.props.history.push('/login')
         // });
     };
     popoverHide = () => {
