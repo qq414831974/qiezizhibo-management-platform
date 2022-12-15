87be3b3fc80724acdfcbc74f21bf66b8e8fc3f27
nivet <fun5xxx@gmail.com>
2022-12-15 14:38:54 +0800

    adjust qiezizhibo-nginx

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,3 @@ MAINTAINER wufan
 RUN mkdir -p /usr/share/nginx/html/manage/
 COPY ./build/  /usr/share/nginx/html/manage/
 COPY ./nginx.conf /etc/nginx/nginx.conf
-RUN mkdir -p /etc/nginx/
-COPY ./tls.crt  /etc/nginx/tls.crt
-COPY ./tls.key  /etc/nginx/tls.key
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 mkdir /usr/local/nginx/log -p
 
-docker run --name nginx -d -p 443:443 -p 80:80 -v /usr/local/nginx/log:/var/log/nginx qiezitv/qiezizhibo-manage:20221129
+docker run --name qiezizhibo-manage -d -p 8081:8081 -v /usr/local/nginx/log:/var/log/nginx qiezitv/qiezizhibo-manage:20221215
